1d26d134807b81fded3bd52744e89b7ecbcea690
Виноградов Евгений <Vinogradov_e_v@rambler.ru>
2022-08-01 10:16:21 +0300

    edited class LoginPage and tests

--- src/test/java/ru/netology/testmode/data/LoginPage.java
+++ src/test/java/ru/netology/testmode/data/LoginPage.java
@@ -3,9 +3,22 @@ package ru.netology.testmode.data;
 import static com.codeborne.selenide.Selenide.$;
 
 public class LoginPage {
-    public static void login(DataGenerator.RegistrationDto user) {
+
+    public void login(DataGenerator.RegistrationDto user) {
         $("[name=login]").setValue(user.getLogin());
         $("[name=password]").setValue(user.getPassword());
+        $("[data-test-id=action-login]").click();
+    }
+
+    public void loginWrongLogin(DataGenerator.RegistrationDto user, String wrongLogin) {
+        $("[name=login]").setValue(wrongLogin);
+        $("[name=password]").setValue(user.getPassword());
+        $("[data-test-id=action-login]").click();
+    }
+
+    public void loginWrongPassword(DataGenerator.RegistrationDto user, String wrongPassword) {
+        $("[name=login]").setValue(user.getLogin());
+        $("[name=password]").setValue(wrongPassword);
         $("[data-test-id=action-login]").click();
     }
 }
--- src/test/java/ru/netology/testmode/test/AuthTest.java
+++ src/test/java/ru/netology/testmode/test/AuthTest.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
 import ru.netology.testmode.data.DataGenerator;
 import ru.netology.testmode.data.LoginPage;
 
@@ -18,6 +19,7 @@ import static ru.netology.testmode.data.DataGenerator.getRandomPassword;
 
 class AuthTest {
 
+    LoginPage loginPage = new LoginPage();
 
     @BeforeEach
     void setup() {
@@ -28,12 +30,8 @@ class AuthTest {
     @DisplayName("Should successfully login with active registered user")
     void shouldSuccessfulLoginIfRegisteredActiveUser() {
         var registeredUser = getRegisteredUser("active");
-        // TODO: добавить логику теста, в рамках которого будет выполнена попытка входа в личный кабинет с учётными
-        //  данными зарегистрированного активного пользователя, для заполнения полей формы используйте
-        //  пользователя registeredUser
-
         DataGenerator.sendRequest(registeredUser);
-        LoginPage.login(registeredUser);
+        loginPage.login(registeredUser);
         $x("//*[contains(text(),'Личный кабинет')]").shouldBe(Condition.visible);
     }
 
@@ -41,10 +39,7 @@ class AuthTest {
     @DisplayName("Should get error message if login with not registered user")
     void shouldGetErrorIfNotRegisteredUser() {
         var notRegisteredUser = getUser("active");
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет
-        //  незарегистрированного пользователя, для заполнения полей формы используйте пользователя notRegisteredUser
-
-        LoginPage.login(notRegisteredUser);
+        loginPage.login(notRegisteredUser);
         $x("//*[text()='Неверно указан логин или пароль']").shouldBe(Condition.visible);
     }
 
@@ -52,11 +47,8 @@ class AuthTest {
     @DisplayName("Should get error message if login with blocked registered user")
     void shouldGetErrorIfBlockedUser() {
         var blockedUser = getRegisteredUser("blocked");
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет,
-        //  заблокированного пользователя, для заполнения полей формы используйте пользователя blockedUser
-
         DataGenerator.sendRequest(blockedUser);
-        LoginPage.login(blockedUser);
+        loginPage.login(blockedUser);
         $x("//*[text()='Пользователь заблокирован']").shouldBe(Condition.visible, Duration.ofSeconds(10));
     }
 
@@ -65,14 +57,8 @@ class AuthTest {
     void shouldGetErrorIfWrongLogin() {
         var registeredUser = getRegisteredUser("active");
         var wrongLogin = getRandomLogin();
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
-        //  логином, для заполнения поля формы "Логин" используйте переменную wrongLogin,
-        //  "Пароль" - пользователя registeredUser
-
         DataGenerator.sendRequest(registeredUser);
-        $("[name=login]").setValue(wrongLogin);
-        $("[name=password]").setValue(registeredUser.getPassword());
-        $("[data-test-id=action-login]").click();
+        loginPage.loginWrongLogin(registeredUser, wrongLogin);
         $x("//*[text()='Неверно указан логин или пароль']").shouldBe(Condition.visible);
     }
 
@@ -81,18 +67,12 @@ class AuthTest {
     void shouldGetErrorIfWrongPassword() {
         var registeredUser = getRegisteredUser("active");
         var wrongPassword = getRandomPassword();
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
-        //  паролем, для заполнения поля формы "Логин" используйте пользователя registeredUser,
-        //  "Пароль" - переменную wrongPassword
-
         DataGenerator.sendRequest(registeredUser);
-        $("[name=login]").setValue(registeredUser.getLogin());
-        $("[name=password]").setValue(wrongPassword);
-        $("[data-test-id=action-login]").click();
+        loginPage.loginWrongLogin(registeredUser, wrongPassword);
         $x("//*[text()='Неверно указан логин или пароль']").shouldBe(Condition.visible);
     }
 }
 
-    // Время, затраченное на ручное тестирование (минут): 20
-    // Время, затраченное на автоматизацию (минут): 240 - большую часть разбирался с кодом, селекторами.
-        // Сами тесты писал минут 10 (с проверкой), благо, заготовки были))
+// Время, затраченное на ручное тестирование (минут): 20
+// Время, затраченное на автоматизацию (минут): 240 - большую часть разбирался с кодом, селекторами.
+// Сами тесты писал минут 10 (с проверкой), благо, заготовки были))
